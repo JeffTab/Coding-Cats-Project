e80c55b6d80e7d8d56782e4d456b0ec30a8fb6db
Rodny Excellent <rodnyexcellent@gmail.com>
2020-01-15 22:14:05 -0500

    got the happy modal working, need to link to the other two emotions, thought out and started to code apikey for spotify/movie but got distracted with homework

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ provider.addScope('email');
 firebase
   .auth()
   .signInWithPopup(provider)
-  .then(function(result) {
+  .then(function (result) {
     // This gives you a Google Access Token.
     var token = result.credential.accessToken;
     // The signed-in user info.
@@ -19,12 +19,37 @@ firebase
 
 
 var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
-var emotion = 
+var emotion =
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+
+
+  });
+
+//modals
+
+fetch("https://accounts.spotify.com/authorize?client_id=audio-analysis/6EJiVf7U0p1BBfs0qqeb1f", {
+  method: "GET",
+  headers: {
+    Authorization: `Bearer ${userAccessToken}`     
+  }
+})
+.then(response => response.json())
+.then(({beats})) => {
+  beats.forEach((beat, index) => {
+    console.log(`Beat ${index} starts at ${beat.start}`);
+  })
+}
+  var searchOMDB = function(movie) {
+    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-      console.log(response);
-
-
+      createRow(response);
     });
+  };
+};
