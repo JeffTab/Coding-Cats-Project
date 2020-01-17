0b37b911f8b0622e3fc9f561aa49fad8f14d30ab
Dayna Knight <dknighted11@gmail.com>
2020-01-16 21:17:06 -0500

    working on otpions page

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
 
-var provider = new firebase.auth.GoogleAuthProvider();
-provider.addScope('profile');
-provider.addScope('email');
-firebase
-  .auth()
-  .signInWithPopup(provider)
-  .then(function(result) {
-    // This gives you a Google Access Token.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // save data to localstorage
-    localStorage.setItem('token', token);
-    localStorage.setItem('user', JSON.stringify(user));
-    // redirect to next page
-    location.replace('<next-page.html>')
-  });
-
 
 var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
 var emotion = 
