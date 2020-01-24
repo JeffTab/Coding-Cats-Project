a5b67a67d4a6b087a72f135cd3714920d9dcd482
Dayna Knight <dknighted11@gmail.com>
2020-01-23 19:34:08 -0500

    giphy finallydisplaying on page.

--- js/script.js
+++ js/script.js
@@ -53,8 +53,19 @@ function runGiphy() {
     method: "GET"
   }).then(function (response) {
     console.log(response);
+
+  for (var i = 0; i < 11 ;i++) {
+
+    var gifURL = response.data[i].images.downsized_medium.url; 
+
+    var gifImage = $("<img>");
+
+    gifImage.attr("src", gifURL);
+    
+    
+    $(".giphy-container").append(gifImage);
+  };
   });
-  $(".giphy-container").append(mood)
 };
 
 function runSpotify() {
@@ -81,4 +92,3 @@ function openMenu() {
     navBarMenu.addClass("is-active");
   };
 };
-
